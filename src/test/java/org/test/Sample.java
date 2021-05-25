70e7e6ebd4517ded2f9731184b57e85c6bc67661
Tamilarasanram <ramtamilarasan096@gmail.com>
2021-05-25 19:14:12 +0530

    New Updated

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Sample {
+	
+	public void carName() {
+		String s = "Audi";
+				System.out.println(s);
+	}
+	public void modelName() {
+		String s1 = "A8";
+		System.out.println(s1);
+}
+}
